0aa17900c7794238bc7b5f83f871d225cfcf32a6
S.M. Sadat Kabir <109563210+smsadatkabir@users.noreply.github.com>
2022-12-02 17:41:48 +0600

    Add files via upload

---
+++ PatternCode/HalfPyramidAfter180Rotation.cpp
@@ -0,0 +1,22 @@
+//make Half Pyramid After 180 Degree Rotation 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(j<=n-i){
+                cout<<" ";
+            }
+            else{
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+
+}
