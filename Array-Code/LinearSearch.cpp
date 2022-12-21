52953274bd289b49b504f8baf711b4c096bcf726
S.M. Sadat Kabir <109563210+smsadatkabir@users.noreply.github.com>
2022-12-22 01:53:55 +0600

    Add files via upload

---
+++ Array-Code/LinearSearch.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int LinearSearch(int array[],int n,int key){
+    for(int i=0;i<n;i++){
+        if(array[i]==key)
+        return i;
+    }
+    return -1;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int array[n];
+    for(int i=0;i<n;i++){
+        cin>>array[i];
+    }
+    int key;
+    cin>>key;
+    cout<<LinearSearch(array,n,key)<<endl;
+
+
+    return 0;
+}
